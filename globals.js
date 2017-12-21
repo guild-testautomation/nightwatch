2279577f8c53cb721c39f56796c374103eadcad2
erikvosseberg <34735984+erikvosseberg@users.noreply.github.com>
2017-12-21 09:35:54 +0100

    eslint fixes

--- globals.js
+++ globals.js
@@ -5,20 +5,20 @@ const chromedriver = require('chromedriver');
 const htmlReporter = require('nightwatch-html-reporter');
 const reporter = new htmlReporter({
     openBrowser: true,
-    reportsDirectory: __dirname + '/reports'
+    reportsDirectory: `${__dirname}/reports`
 });
 
 // Now we can define the driver to start before a test and close after.
 module.exports = {
-  before : function(done) {
-    chromedriver.start();
-    done();
-  },
+    before : function(done) {
+        chromedriver.start();
+        done();
+    },
 
-  after : function(done) {
-    chromedriver.stop();
+    after : function(done) {
+        chromedriver.stop();
 
-    done();
-  },
-  reporter: reporter.fn
-};  
+        done();
+    },
+    reporter: reporter.fn
+};
